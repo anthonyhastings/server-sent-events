1ed146081e2623b8c8b33ef749da0ccf3d1e2272
Anthony Hastings <ar.hastings@gmail.com>
2023-10-30 22:30:45 +0000

    adding shared types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+export type Item = z.infer<typeof ItemSchema>;
+export const ItemSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  price: z.number(),
+});
+
+export type Order = z.infer<typeof OrderSchema>;
+export const OrderSchema = z.object({
+  id: z.string(),
+  items: z.array(ItemSchema),
+  status: z.enum(['PENDING', 'DELIVERED']),
+});
+
+export type LatLng = z.infer<typeof LatLngSchema>;
+export const LatLngSchema = z.object({
+  lat: z.number(),
+  lng: z.number(),
+});
+
+export type Route = z.infer<typeof RouteSchema>;
+export const RouteSchema = z.object({
+  id: z.string(),
+  currentLatLng: LatLngSchema.nullable(),
+  currentOrderId: z.string().nullable(),
+});
