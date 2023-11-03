e33a970076b4d5f29f59e589845d271ad94075e8
Anthony Hastings <ar.hastings@gmail.com>
2023-11-03 22:01:18 +0000

    adding react-query

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -1,27 +1,28 @@
 import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { envVars } from './env';
 import { MapLoader, Map, useMapContext } from '@/components/map';
 import { Marker } from '@/components/marker';
+import { CACHE_TIMES } from '@/constants';
 import './App.css';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            staleTime: CACHE_TIMES.NORMAL,
+          },
+        },
+      }),
+  );
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </QueryClientProvider>
   );
 };
 
